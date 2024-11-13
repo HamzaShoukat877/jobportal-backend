ae2923d25bed71dc20096fa8c99975c43349a568
Hamilton S <amentotech009@gmail.com>
2024-11-13 17:40:29 +0500

    work on token

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -111,7 +111,7 @@ export const login = async (req, res) => {
         const options = {
             maxAge: 1 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            samesite: 'strict'
+            samesite: 'none'
         }
 
         return res.status(200).cookie("token", token, options).json({
